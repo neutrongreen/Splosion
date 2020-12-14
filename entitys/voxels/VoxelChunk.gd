69180e064b8c3d0b411c5a57552111732551ca43
neutrongreen <joshua@green.geek.nz>
2020-12-14 19:10:35 +1300

    cleanup code

--- entitys/voxels/VoxelChunk.gd
+++ entitys/voxels/VoxelChunk.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-export var chunk_pos= Vector2(0 ,0)
+export var chunk_pos = Vector2(0, 0)
 var map = NAN
 var polygons = []
 var bitwise = [1, 2, 4, 8]
@@ -10,44 +10,49 @@ var test_colours = NAN
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var edges = [[Vector2(0, 0), Vector2(1, 0)], 
-				[Vector2(1, 0), Vector2(1, 1)], 
-				[Vector2(0, 1), Vector2(1, 1)], 
-				[Vector2(0, 0), Vector2(0, 1)]]
-var edgesaxis = [
-	0,
-	1,
-	0,
-	1
+var edges = [
+	[Vector2(0, 0), Vector2(1, 0)],
+	[Vector2(1, 0), Vector2(1, 1)],
+	[Vector2(0, 1), Vector2(1, 1)],
+	[Vector2(0, 0), Vector2(0, 1)]
 ]
-func interpolateVertsY(var v1, var v2, var vect1, var vect2):
-	vect1.y = vect1.y + ((map.SURFACE_LEVEL-v1)/(v2-v1))*(vect2.y - vect1.y)
+var edgesaxis = [0, 1, 0, 1]
+
+
+func interpolateVertsY(v1, v2, vect1, vect2):
+	vect1.y = vect1.y + ((map.SURFACE_LEVEL - v1) / (v2 - v1)) * (vect2.y - vect1.y)
 	vect1.y = clamp(vect1.y, 0, 1)
 	return vect1.y
-func interpolateVertsX(var v1, var v2, var vect1, var vect2):
-	vect1.x = vect1.x + ((map.SURFACE_LEVEL-v1)/(v2-v1))*(vect2.x - vect1.x)
+
+
+func interpolateVertsX(v1, v2, vect1, vect2):
+	vect1.x = vect1.x + ((map.SURFACE_LEVEL - v1) / (v2 - v1)) * (vect2.x - vect1.x)
 	vect1.x = clamp(vect1.x, 0, 1)
 	return vect1.x
+
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 # Called when the node enters the scene tree for the first time.
 #flip axis commands
-func flip_x_axis(var vector):
+func flip_x_axis(vector):
 	if vector.x == 1:
-		vector.x = 0 
+		vector.x = 0
 	elif vector.x == 0:
 		vector.x = 1
 	return vector
 
-func flip_y_axis(var vector):
+
+func flip_y_axis(vector):
 	if vector.y == 1:
-		vector.y = 0 
+		vector.y = 0
 	elif vector.y == 0:
 		vector.y = 1
 	return vector
 
-func get_shape(var VOXEL_DATA, var mappos):
+
+func get_shape(VOXEL_DATA, mappos):
 	#set shape to voxel data shape
 	var shape = map.SHAPE_LIST.SHAPES[VOXEL_DATA.SHAPE_ID]
 	#for i in range of shape size
@@ -56,22 +61,30 @@ func get_shape(var VOXEL_DATA, var mappos):
 		#flip x axis
 		if VOXEL_DATA.FLIP_X:
 			tempvector = flip_x_axis(tempvector)
-		
+
 		#flip y axis
 		if VOXEL_DATA.FLIP_Y:
 			tempvector = flip_y_axis(tempvector)
-		
+
 		#apply interpolation if vertex is = edge
 		for edge in VOXEL_DATA.INTERPOLATED_EDGES:
 			if edge.x == i:
 				var b = edges[edge.y][1]
 				var d = edges[edge.y][0]
 				if edgesaxis[edge.y] == 0:
-					tempvector.x = interpolateVertsX(map.map[d.x + mappos.x][d.y + mappos.y], 
-					map.map[b.x + mappos.x][b.y + mappos.y], d, b)
+					tempvector.x = interpolateVertsX(
+						map.map[d.x + mappos.x][d.y + mappos.y],
+						map.map[b.x + mappos.x][b.y + mappos.y],
+						d,
+						b
+					)
 				else:
-					tempvector.y = interpolateVertsY(map.map[d.x + mappos.x][d.y + mappos.y], 
-					map.map[b.x + mappos.x][b.y + mappos.y], d, b)
+					tempvector.y = interpolateVertsY(
+						map.map[d.x + mappos.x][d.y + mappos.y],
+						map.map[b.x + mappos.x][b.y + mappos.y],
+						d,
+						b
+					)
 				break
 		tempvector += mappos
 		tempvector *= map.TILE_SIZE
@@ -81,31 +94,34 @@ func get_shape(var VOXEL_DATA, var mappos):
 		shape.set(i, tempvector)
 	return shape
 
+
 func generate_voxel(pos):
 	var voxid = 0
 	var mappos = pos
 	#get location value
 	if map.map[mappos.x][mappos.y] > map.SURFACE_LEVEL:
 		voxid |= bitwise[0]
-	if map.map[mappos.x+1][mappos.y] > map.SURFACE_LEVEL:
+	if map.map[mappos.x + 1][mappos.y] > map.SURFACE_LEVEL:
 		voxid |= bitwise[1]
-	if map.map[mappos.x][mappos.y+1] > map.SURFACE_LEVEL:
+	if map.map[mappos.x][mappos.y + 1] > map.SURFACE_LEVEL:
 		voxid |= bitwise[2]
-	if map.map[mappos.x+1][mappos.y+1] > map.SURFACE_LEVEL:
+	if map.map[mappos.x + 1][mappos.y + 1] > map.SURFACE_LEVEL:
 		voxid |= bitwise[3]
 	for i in map.VOXEL_TABLE:
 		if i.points == voxid:
-			return get_shape(i, Vector2(mappos.x,mappos.y))
+			return get_shape(i, Vector2(mappos.x, mappos.y))
+
 
 func update_voxels():
 	polygons = []
 	for x in range(map.CHUNK_SIZE):
 		polygons.append([])
 		for y in range(map.CHUNK_SIZE):
-			var dis = Vector2(chunk_pos.x*map.CHUNK_SIZE + x, chunk_pos.y*map.CHUNK_SIZE + y)
+			var dis = Vector2(chunk_pos.x * map.CHUNK_SIZE + x, chunk_pos.y * map.CHUNK_SIZE + y)
 			#generate polygon for chunk location
 			polygons[x].append(generate_voxel(dis))
 	update()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -113,7 +129,7 @@ func _ready():
 	#https://godotengine.org/qa/2539/how-would-i-go-about-picking-a-random-number
 	var random_generator = RandomNumberGenerator.new()
 	random_generator.randomize()
-	var random_value = random_generator.randf_range(0.00,60.00)
+	var random_value = random_generator.randf_range(0.00, 60.00)
 	test_colours = PoolColorArray([Color.from_hsv(random_value, 0.5, 1)])
 	create_shape_owner(self)
 	update_voxels()
@@ -126,18 +142,19 @@ func _ready():
 #called when master render function is called and redraws on the screen the chunk
 #and redraws the collsion meshes 
 
+
 func _draw():
 	shape_owner_clear_shapes(0)
 	for x in range(map.CHUNK_SIZE):
 		for y in range(map.CHUNK_SIZE):
-			if(polygons[x][y]):
+			if polygons[x][y]:
 				var geo_invalid = Geometry.triangulate_polygon(polygons[x][y]).empty()
-				if(!geo_invalid):
+				if ! geo_invalid:
 #					if map.testing:
 #						draw_polygon(polygons[x][y], test_colours)
 #					else:
 					draw_polygon(polygons[x][y], colours)
-					
+
 					var collision = ConvexPolygonShape2D.new()
 					collision.set_point_cloud(polygons[x][y])
 					shape_owner_add_shape(0, collision)
--- entitys/voxels/VoxelMap.gd
+++ entitys/voxels/VoxelMap.gd
@@ -19,20 +19,17 @@ export (Vector2) var MAP_SIZE = Vector2(4, 4)
 export (Vector2) var SPAWN_OFFSET = Vector2(4, 4)
 export (float) var MAP_SCALE = 1
 export (float) var SURFACE_LEVEL = 0.4
-export (bool) var testing = false;
+export (bool) var testing = false
 # Called when the node enters the scene tree for the first time.
 var map = []
 onready var chunks = []
-var edges = [[Vector2(0, 0), Vector2(1, 0)], 
-				[Vector2(1, 0), Vector2(1, 1)], 
-				[Vector2(0, 1), Vector2(1, 1)], 
-				[Vector2(0, 0), Vector2(0, 1)]]
-var edgesaxis = [
-	0,
-	1,
-	0,
-	1
+var edges = [
+	[Vector2(0, 0), Vector2(1, 0)],
+	[Vector2(1, 0), Vector2(1, 1)],
+	[Vector2(0, 1), Vector2(1, 1)],
+	[Vector2(0, 0), Vector2(0, 1)]
 ]
+var edgesaxis = [0, 1, 0, 1]
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -44,14 +41,15 @@ static func delete_children(node):
 		node.remove_child(n)
 		n.queue_free()
 
+
 func generate_map(persist):
-	if !persist:
+	if ! persist:
 		map = []
-		for x in range((MAP_SIZE.x*CHUNK_SIZE) + 1):
+		for x in range((MAP_SIZE.x * CHUNK_SIZE) + 1):
 			map.append([])
-			for y in range((MAP_SIZE.y*CHUNK_SIZE) + 1):
-				map[x].append(noise.get_noise_2d(x*MAP_SCALE, y*MAP_SCALE) + 1)
-	if(!chunks):
+			for y in range((MAP_SIZE.y * CHUNK_SIZE) + 1):
+				map[x].append(noise.get_noise_2d(x * MAP_SCALE, y * MAP_SCALE) + 1)
+	if ! chunks:
 		for x in range(MAP_SIZE.x):
 			chunks.append([])
 			for y in range(MAP_SIZE.y):
@@ -63,7 +61,8 @@ func generate_map(persist):
 			chunks.append([])
 			for y in range(MAP_SIZE.y):
 				chunks[x][y].update_voxels()
-			
+
+
 func get_updated_chunks(coords):
 	var x_array = []
 	var y_array = []
@@ -78,19 +77,23 @@ func get_updated_chunks(coords):
 		y_array.append(floor(coords.y) - 1)
 	for x in x_array:
 		for y in y_array:
-			if !(x < 0) and x < MAP_SIZE.x:
-				if !(y < 0) and y < MAP_SIZE.y:
+			if ! (x < 0) and x < MAP_SIZE.x:
+				if ! (y < 0) and y < MAP_SIZE.y:
 					return_array.append(Vector2(x, y))
 	return return_array
-	
+
+
 #https://stackoverflow.com/questions/15856411/finding-all-the-points-within-a-circle-in-2d-space
 func circle_brush(mappos, r, strength):
 	var updated_chunks = []
 	for x in range(mappos.x - r, mappos.x + r):
-		var yspan = floor(r*sin(acos((mappos.x-x)/r)));
+		var yspan = floor(r * sin(acos((mappos.x - x) / r)))
 		for y in range(mappos.y - yspan, mappos.y + yspan):
 			var i = Vector2(x, y)
-			if !((i.y > CHUNK_SIZE*MAP_SIZE.y) or i.y < 0) and !((i.x > CHUNK_SIZE*MAP_SIZE.x) or i.x < 0):
+			if (
+				! ((i.y > CHUNK_SIZE * MAP_SIZE.y) or i.y < 0)
+				and ! ((i.x > CHUNK_SIZE * MAP_SIZE.x) or i.x < 0)
+			):
 				#update map location
 				map[i.x][i.y] = strength
 				#then get what chunks are updated and if not in the update list add them
@@ -101,14 +104,16 @@ func circle_brush(mappos, r, strength):
 						if c == f:
 							is_dup = true
 							break
-					if !is_dup:
+					if ! is_dup:
 						updated_chunks.append(c)
 	for i in updated_chunks:
 		chunks[i.x][i.y].update_voxels()
 		#recoulor if updetated
+
+
 #		chunks[i.x][i.y].colours = PoolColorArray([Color(255,255,0)])
-				#(x, y), (x, ySym), (xSym , y), (xSym, ySym) are in the circle
-		
+#(x, y), (x, ySym), (xSym , y), (xSym, ySym) are in the circle
+
 
 func _ready():
 	randomize()
@@ -119,14 +124,22 @@ func _ready():
 	noise.persistence = 0.8
 	generate_map(false)
 	#extra code for player postioning
-	circle_brush(Vector2((CHUNK_SIZE*MAP_SIZE.x)/2 + SPAWN_OFFSET.x, SPAWN_OFFSET.y), 10, 0)
+	circle_brush(Vector2((CHUNK_SIZE * MAP_SIZE.x) / 2 + SPAWN_OFFSET.x, SPAWN_OFFSET.y), 10, 0)
+
 
 #optional code
 func _draw():
-	var mesh = PoolVector2Array([Vector2(0, TILE_SIZE*CHUNK_SIZE*MAP_SIZE.y-endbar_size), 
-		Vector2( TILE_SIZE*CHUNK_SIZE*MAP_SIZE.x, TILE_SIZE*CHUNK_SIZE*MAP_SIZE.y-endbar_size),
-		Vector2( TILE_SIZE*CHUNK_SIZE*MAP_SIZE.x, TILE_SIZE*CHUNK_SIZE*MAP_SIZE.y), 
-		Vector2(0, TILE_SIZE*CHUNK_SIZE*MAP_SIZE.y)])
+	var mesh = PoolVector2Array(
+		[
+			Vector2(0, TILE_SIZE * CHUNK_SIZE * MAP_SIZE.y - endbar_size),
+			Vector2(
+				TILE_SIZE * CHUNK_SIZE * MAP_SIZE.x,
+				TILE_SIZE * CHUNK_SIZE * MAP_SIZE.y - endbar_size
+			),
+			Vector2(TILE_SIZE * CHUNK_SIZE * MAP_SIZE.x, TILE_SIZE * CHUNK_SIZE * MAP_SIZE.y),
+			Vector2(0, TILE_SIZE * CHUNK_SIZE * MAP_SIZE.y)
+		]
+	)
 	draw_polygon(mesh, PoolColorArray([Color("32CD32")]))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -139,7 +152,7 @@ func _draw():
 #	elif Input.is_action_pressed("ui_down"):
 #		SURFACE_LEVEL -= 0.1*delta
 #		generate_map(true)
-	
+
 #	if Input.is_action_just_pressed("ui_select"):
 #		var tempball = Ball.instance()
 #		tempball.position = get_global_mouse_position()
